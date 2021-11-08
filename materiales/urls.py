6f2631051d1a2dc565ae0f360a5e9ee69fc7013c
SebastianNarvaez11 <narvaez.jhoan@correounivalle.edu.co>
2021-11-08 11:42:37 -0500

    add telas

--- materiales/urls.py
+++ materiales/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path('', views.materiales, name='materiales'),
     path('<slug:slug_material>/', views.detallematerial, name='detallematerial'),
     path('telas/prana/', views.prana, name='prana'),
+    path('telas/cuerotext/', views.cuerotext, name='cuerotext'),
+    path('telas/bisont/', views.bisont, name='bisont'),
+    path('telas/capadocia/', views.capadocia, name='capadocia'),
 ]
 
 materiales_urldash = ([
     path('list/', MaterialListView.as_view(), name='list'),
-    path('create/',MaterialCreateView.as_view(), name='create'),
+    path('create/', MaterialCreateView.as_view(), name='create'),
     path('update/<int:pk>/', MaterialUpdateView.as_view(), name='update'),
     path('delete/<int:pk>/', MaterialDeleteView.as_view(), name='delete')
 ], 'materiales_dash')
--- materiales/views.py
+++ materiales/views.py
@@ -41,7 +41,7 @@ def materiales(request):
             except:
                 return redirect(reverse('materiales')+"?fail")
 
-    return render(request, 'materiales/materiales.html', {'telas': telas, 'maderas' : maderas, 'pinturas' : pinturas, 'formulario': suscribe_form})
+    return render(request, 'materiales/materiales.html', {'telas': telas, 'maderas': maderas, 'pinturas': pinturas, 'formulario': suscribe_form})
 
 
 def detallematerial(request, slug_material):
@@ -73,7 +73,6 @@ def detallematerial(request, slug_material):
     return render(request, 'materiales/detallematerial.html', {'material': material, 'formulario': suscribe_form})
 
 
-
 def prana(request):
 
     suscribe_form = SuscribeForm()
@@ -99,8 +98,91 @@ def prana(request):
             except:
                 return redirect(reverse('materiales')+"?fail")
 
-    return render(request, 'materiales/prana.html',{'formulario': suscribe_form})
+    return render(request, 'materiales/prana.html', {'formulario': suscribe_form})
 
+
+def cuerotext(request):
+
+    suscribe_form = SuscribeForm()
+    if request.method == "POST":
+        # Optiene los datos del formulario
+        suscribe_form = SuscribeForm(data=request.POST)
+        if suscribe_form.is_valid():
+            asunto = 'Solicitud de Suscripcion'
+            email = request.POST.get('email', '')
+            contenido = 'Hola, me gustaria suscribirme a su lista de correos electronicos'
+            # ENVIAMOS EL CORREO
+            email = EmailMessage(
+                "Sitio Web - {}".format(asunto),  # Asunto del mensaje
+                "Email: <{}> \n\nEscribio: \n\n{} ".format(
+                    email, contenido),  # estructura del mensaje
+                "testing.developer.404@gmail.com",  # email de origen
+                ["narvaez.jhoan@correounivalle.edu.co"],  # email de destino
+                reply_to=[email]
+            )
+            try:
+                email.send()
+                return redirect(reverse('materiales')+"?ok")
+            except:
+                return redirect(reverse('materiales')+"?fail")
+
+    return render(request, 'materiales/cuerotext.html', {'formulario': suscribe_form})
+
+
+def bisont(request):
+
+    suscribe_form = SuscribeForm()
+    if request.method == "POST":
+        # Optiene los datos del formulario
+        suscribe_form = SuscribeForm(data=request.POST)
+        if suscribe_form.is_valid():
+            asunto = 'Solicitud de Suscripcion'
+            email = request.POST.get('email', '')
+            contenido = 'Hola, me gustaria suscribirme a su lista de correos electronicos'
+            # ENVIAMOS EL CORREO
+            email = EmailMessage(
+                "Sitio Web - {}".format(asunto),  # Asunto del mensaje
+                "Email: <{}> \n\nEscribio: \n\n{} ".format(
+                    email, contenido),  # estructura del mensaje
+                "testing.developer.404@gmail.com",  # email de origen
+                ["narvaez.jhoan@correounivalle.edu.co"],  # email de destino
+                reply_to=[email]
+            )
+            try:
+                email.send()
+                return redirect(reverse('materiales')+"?ok")
+            except:
+                return redirect(reverse('materiales')+"?fail")
+
+    return render(request, 'materiales/bisont.html', {'formulario': suscribe_form})
+
+
+def capadocia(request):
+
+    suscribe_form = SuscribeForm()
+    if request.method == "POST":
+        # Optiene los datos del formulario
+        suscribe_form = SuscribeForm(data=request.POST)
+        if suscribe_form.is_valid():
+            asunto = 'Solicitud de Suscripcion'
+            email = request.POST.get('email', '')
+            contenido = 'Hola, me gustaria suscribirme a su lista de correos electronicos'
+            # ENVIAMOS EL CORREO
+            email = EmailMessage(
+                "Sitio Web - {}".format(asunto),  # Asunto del mensaje
+                "Email: <{}> \n\nEscribio: \n\n{} ".format(
+                    email, contenido),  # estructura del mensaje
+                "testing.developer.404@gmail.com",  # email de origen
+                ["narvaez.jhoan@correounivalle.edu.co"],  # email de destino
+                reply_to=[email]
+            )
+            try:
+                email.send()
+                return redirect(reverse('materiales')+"?ok")
+            except:
+                return redirect(reverse('materiales')+"?fail")
+
+    return render(request, 'materiales/capadocia.html', {'formulario': suscribe_form})
 #--------------------Vistas Del Dashboard-----------------------#
 
 
